ca57742da22987313370e322e7e2266643c3cf47
Zhuo Zhang <zhan3299@purdue.edu>
2024-03-30 00:06:52 -0500

    chore: restore files in crate/common and crate/evm to be consistent with foundry-rs

--- crates/evm/coverage/src/analysis.rs
+++ crates/evm/coverage/src/analysis.rs
@@ -52,7 +52,7 @@ impl<'a> ContractVisitor<'a> {
         let kind: String =
             node.attribute("kind").ok_or_else(|| eyre::eyre!("Function has no kind"))?;
         if kind == "constructor" || kind == "receive" {
-            return Ok(());
+            return Ok(())
         }
 
         match node.body.take() {
@@ -430,7 +430,7 @@ impl SourceAnalyzer {
         for (source_id, ast) in asts.into_iter() {
             for child in ast.nodes {
                 if !matches!(child.node_type, NodeType::ContractDefinition) {
-                    continue;
+                    continue
                 }
 
                 let contract_id = ContractId {
